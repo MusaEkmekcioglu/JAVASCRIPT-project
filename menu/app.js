21231eca0608807bfecfa1b6f1b877152c903f68
athensmusa <athensmusa@gmail.com>
2021-08-29 00:35:06 +0300

    m

--- menu/app.js
+++ menu/app.js
@@ -71,4 +71,71 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "steak dinner",
+    category: "dinner",
+    price: 39.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
 ];
+
+const sectionCenter = document.querySelector(".section-center");
+const filterBtns = document.querySelectorAll(".filter-btn");
+
+
+
+window.addEventListener("DOMContentLoaded", function () {
+
+displayMenuItems(menu);
+const categories =menu.map(function(item){
+  return item.category;
+})
+});
+
+
+filterBtns.forEach(function(btn){
+ btn.addEventListener("click",function(e){
+
+    const category= e.currentTarget.dataset.id;
+    const menuCategory=menu.filter(function(menuItem){
+
+      if(menuItem.category === category){
+        return menuItem;
+      }
+     
+    });
+    if(category=="all"){
+      return displayMenuItems(menu);
+    }
+    else{
+      return displayMenuItems(menuCategory);
+    }
+
+});
+
+});
+function displayMenuItems(menuItems){
+  let displayMenu = menuItems.map(function (item) {
+
+    return `<article class="menu-item">
+  <img src="${item.img}" class="photo" alt="${item.title}">
+  <div class="item-info">
+    <header>
+      <h4>${item.title}</h4>
+      <h4 class="price">$${item.price}</h4>
+    </header>
+    <p class="item-text">
+    ${item.desc}
+    </p>
+  </div>
+</article>`
+
+  });
+  
+  displayMenu=displayMenu.join("");
+ 
+  sectionCenter.innerHTML = displayMenu;
+
+}
